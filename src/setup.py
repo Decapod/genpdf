284cfb77aaf79427d87ce6138110e40ef2130974
jhung <jhung@localhost>
2011-11-25 21:20:41 +0100

    NOJIRA: fixed the script to properly install img2pdfper.py and binned-inter

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(name='pdfGeneration',
       version='1.0',
       author = "IUPR",
-      scripts = glob.glob("ocro*") + ["decapod-genpdf.py"],
+      scripts = glob.glob("ocro*") + ["decapod-genpdf.py", "fontGrouper.py", "img2pdfper.py"],
       description = """PDF generation for Decapod
 """,
       py_modules=['ocro2pdf','ocrodir'],
